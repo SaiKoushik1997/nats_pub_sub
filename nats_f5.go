6a9591f9e480bc4cd7f3a1faf03f5e87d5920abd
SaiKoushik1997 <36809251+SaiKoushik1997@users.noreply.github.com>
2019-06-26 23:30:24 +0530

    nats pub sub pkg

---
+++ nats_f5.go
@@ -0,0 +1,60 @@
+package nats_f5
+
+
+import (
+        "fmt"
+        "log"
+	"bufio"
+	"os"
+	"strings"
+        "github.com/nats-io/go-nats"
+)
+
+
+func publish_f5(subject string, msg string) {
+        // [begin publish_bytes]
+        nc, err := nats.Connect(nats.DefaultURL)
+        if err != nil {
+                log.Fatal(err)
+        }
+        fmt.Println("working")
+        //defer nc.Close()
+
+        if err := nc.Publish(subject, []byte(msg)); err != nil {
+                log.Fatal(err)
+        }
+        // [end publish_bytes]
+        // Make sure the message goes through before we close
+        nc.Flush()
+}
+
+
+func subscribe_f5(subject string)string{
+	nc, err := nats.Connect(nats.DefaultURL)
+        if err != nil {
+                log.Fatal(err)
+        }
+        defer nc.Close()
+
+        // Subscribe
+        sub, err := nc.SubscribeSync(subject)
+        if err != nil {
+                log.Fatal(err)
+        }
+	
+	// Wait for a message
+        msg, err := sub.NextMsg(30 * time.Second)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        // Use the response
+        //log.Printf("Reply: %s", msg.Data)
+
+	// Close the connection
+        nc.Close()
+
+	return msg.Data
+
+
+}
